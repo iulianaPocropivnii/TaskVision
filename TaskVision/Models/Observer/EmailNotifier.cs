f237a659643d7393908d8702fd72cedfc5371ef9
iulia <iulia@IULIANA>
2025-05-26 11:14:53 +0300

    add observer pattern

---
+++ TaskVision/Models/Observer/EmailNotifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskVision.Services.Interfaces;
+
+namespace TaskVision.Models.Observer
+{
+    public class EmailNotifier : IObserver
+    {
+        private readonly string _recipientEmail;
+
+        public EmailNotifier(string recipientEmail)
+        {
+            _recipientEmail = recipientEmail;
+        }
+
+        public void Update(string message)
+        {
+            Console.WriteLine($"[EMAIL to {_recipientEmail}]: {message}");
+        }
+    }
+}
---
+++ TaskVision/Models/Observer/PopupNotifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskVision.Services.Interfaces;
+
+namespace TaskVision.Models.Observer
+{
+    public class PopupNotifier : IObserver
+    {
+        public async void Update(string message)
+        {
+            await App.Current.MainPage.DisplayAlert("Notificare Task", message, "OK");
+        }
+    }
+}
